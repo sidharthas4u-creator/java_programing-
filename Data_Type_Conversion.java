a2d9e9a68e9a6c49e541e6b9948b595aa6765bdd
sidharth <sidharthas4u@gmail.com>
2026-08-11 13:11:20 +0530

    Add temperature conversion from Celsius to Fahrenheit

---
+++ Data_Type_Conversion.java
@@ -0,0 +1,7 @@
+public class Data_Type_Conversion {
+    public static void main(String[] args) {
+         int celsius=100;
+         double fahrenheit=(celsius*(9/5))+32.0;
+         System.out.print("Celsius:"+celsius+"\nFahrenheit:"+fahrenheit);
+    }
+}
