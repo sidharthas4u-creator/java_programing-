36d638d1ef0166f30218108bc65e131ec4f07469
sidharth <sidharthas4u@gmail.com>
2026-08-11 13:13:16 +0530

    Implement basic arithmetic operations in Java

---
+++ Arithmetic_Operations.java
@@ -0,0 +1,10 @@
+public class Arithmetic_Operations {
+    public static void main(String[] args) {
+         int a=2,b=3;
+         System.out.println(a+"+"+b+"="+(a+b));
+         System.out.println(a+"-"+b+"="+(a-b));
+         System.out.println(a+"x"+b+"="+(a*b));
+         System.out.println(a+"/"+b+"="+(a/b));
+         System.out.println(a+"%"+b+"="+(a%b));
+    }
+}
