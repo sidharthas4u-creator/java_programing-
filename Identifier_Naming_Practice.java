f28f38b4d875af4329c8d5dd87ae092ec056ed31
sidharth <sidharthas4u@gmail.com>
2026-08-11 13:09:04 +0530

    Add Identifier_Naming_Practice class with main method

---
+++ Identifier_Naming_Practice.java
@@ -0,0 +1,8 @@
+public class Identifier_Naming_Practice {
+    public static void main(String[] args) {
+        String Name ="Sidharth AS";
+        int roll_number=584594235;
+        double mark=68.7;
+        System.out.print("Name:"+Name+"\nRoll Number:"+roll_number+"\nMark:"+mark);
+    }
+}
