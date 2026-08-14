3674c9b85883ea32f05d970d7855cc7541a047de
sidharth <sidharthas4u@gmail.com>
2026-08-14 18:14:06 +0530

    Add Java Stdin and Stdout solution
    Implement Java program to read and print three integers.

---
+++ hacker_rank_solution_java/Java_Stdin_and_Stdout.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int a3 = sc.nextInt();
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
